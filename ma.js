42d026d2812f59685b0e39bdefbdffde504ef072
lucasdpsa01 <lucas.foxy1987@gmail.com>
2024-05-15 09:20:05 -0300

    pao de queijo

---
+++ ma.js
@@ -0,0 +1,30 @@
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 2.68;
+let y = 2.99;
+let n = -3.1;
+let ç = 4;
+let k = 2;
+let i = 45;
+let f = 1;
+let d = 2;
+let v = 3;
+
+z = Math.round(x); // arredonda, se for > 0,50 vai arredontar pra cima e a mesma coisa ao contrário
+w = Math.floor(y); // arredonda para baixo
+q = Math.ceil(x); // arredonda para cima
+e = Math.trunc(y); //tira a parte racional
+r = Math.pow(ç, k); //potencia, ç é a base e o k é o expoente
+t = Math.sqrt(ç); //raiz quadrada
+l = Math.log(2); //logaritmo
+j = Math.sin(i); //seno
+h = Math.cos(i); //coseno
+//Math.tan() --> tangente
+c = Math.abs(n) //negativo para positivo
+g = Math.sign(n) //sinal, se for negativo sai -1, se for positivo sai positivo
+let max = Math.max(f, d, v);
+let min = Math.min(f, d, v);
+
+console.log(g);
